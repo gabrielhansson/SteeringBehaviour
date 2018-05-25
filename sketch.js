ae2b845012d614c7d3c5d4e9152a04d80b6110e4
Gabriel Hansson <gabrielhansson00@gmail.com>
2018-05-26 00:59:57 +0200

    Addet support for multiple organisms Improved the meals object and refactored the regarding code for support for multiple meals. Also started by experiment with dna and a attraction or reppelent for some meals.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
-let organism
-let meals = {
-	snacks: [],
-	poison: [],
-	radius: 15
-}
+let organisms = []
+let meals = [
+	{
+		name: 'food',
+		healthyness: 100,
+		pieces: [],
+		radius: 15,
+		color: {
+			r: 0,
+			g: 255,
+			b: 0
+		}
+	},
+	{
+		name: 'food',
+		healthyness: -50,
+		pieces: [],
+		radius: 15,
+		color: {
+			r: 255,
+			g: 0,
+			b: 0
+		}
+	}
+]
 
 
 
@@ -11,28 +30,31 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noStroke()
 
-	organism = new Organism()
+	//TEMP creates 10 new organisms
+	for (let i = 0; i < 10; i++){
+			organisms[i] = new Organism()
+	}
 
-	//TEMP
-	for (let meal in meals){
-		while(meals[meal].length <= 10){
-			meals[meal].push(createVector(random(windowWidth), random(windowHeight)))
+	//TEMP creates 10 new pieces from each meal
+	for (let meal of meals){
+		console.log(meal)
+		while(meal.pieces.length <= 10){
+			meal.pieces.push(createVector(random(windowWidth), random(windowHeight)))
 		}
 	}
 }
 
 function draw() {
 	background(255);
-
-	organism.eat(meals)
+	for (let organism of organisms)	organism.eat(meals)
  	//draw the eatable things
-	fill(0, 255, 0)
-	for (let snack of meals.snacks){
-		ellipse(snack.x, snack.y, meals.radius, meals.radius)
-	}
-
-	fill(255, 0, 0)
-	for (let bad of meals.poison){
-		ellipse(bad.x, bad.y, meals.radius, meals.radius)
+	for (let meal in meals){
+		let r = meals[meal].color.r
+		let g = meals[meal].color.g
+		let b = meals[meal].color.b
+		fill(r, g, b)
+		for (let piece of meals[meal].pieces){
+			ellipse(piece.x, piece.y, meals[meal].radius, meals[meal].radius)
+		}
 	}
 }
